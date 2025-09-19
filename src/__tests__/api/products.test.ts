f879de5fb3655faa3c22a2cd02b7d01699dfb282
Abe Estrada <abe@estrada.email>
2025-09-19 13:05:07 -0600

    refactor(api): remove delay implementation
    The delay will be simulated on the frontend

--- src/__tests__/api/products.test.ts
+++ src/__tests__/api/products.test.ts
@@ -5,7 +5,6 @@
 import { GET } from "@/app/api/products/route";
 import { getProducts } from "@/lib/cache";
 import type { ProductsInterface } from "@/interfaces/Products";
-import type { NextRequest } from "next/server";
 
 jest.mock("@/lib/cache");
 
@@ -30,49 +29,11 @@ describe("/api/products", () => {
 
 		(getProducts as jest.Mock).mockResolvedValueOnce(data);
 
-		const mockRequest = {
-			nextUrl: {
-				searchParams: new URLSearchParams(),
-			},
-		} as NextRequest;
-
-		const response = await GET(mockRequest);
+		const response = await GET();
 		const json = await response.json();
 
 		expect(response.status).toBe(200);
 		expect(json?.products?.length).toBeGreaterThan(0);
-	});
-
-	it("should handle delay query parameter", async () => {
-		const data: ProductsInterface = {
-			products: [
-				{
-					id: 1,
-					title: "Product",
-					description: "The quick brown fox jumps over the lazy dog",
-				},
-			],
-			total: 1,
-			skip: 0,
-			limit: 1,
-		};
-
-		(getProducts as jest.Mock).mockResolvedValueOnce(data);
-
-		const mockRequest = {
-			nextUrl: {
-				searchParams: new URLSearchParams("delay=10"),
-			},
-		} as NextRequest;
-
-		const startTime = Date.now();
-		const response = await GET(mockRequest);
-		const endTime = Date.now();
-		const json = await response.json();
-
-		expect(response.status).toBe(200);
-		expect(json?.products?.length).toBeGreaterThan(0);
-		expect(endTime - startTime).toBeGreaterThanOrEqual(5);
 	});
 
 	it("should return 500 if getProducts throws an error", async () => {
@@ -80,13 +41,7 @@ describe("/api/products", () => {
 			new Error("Database connection failed"),
 		);
 
-		const mockRequest = {
-			nextUrl: {
-				searchParams: new URLSearchParams(),
-			},
-		} as NextRequest;
-
-		const response = await GET(mockRequest);
+		const response = await GET();
 		const json = await response.json();
 
 		expect(response.status).toBe(500);
--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,15 +1,6 @@
 import { getProducts } from "@/lib/cache";
-import type { NextRequest } from "next/server";
 
-export async function GET(request: NextRequest) {
-	const searchParams = request.nextUrl.searchParams;
-
-	const delay = searchParams.get("delay");
-	if (delay && !Number.isNaN(Number(delay))) {
-		const delayMs = parseInt(delay, 10);
-		await new Promise<void>((res) => setTimeout(res, delayMs));
-	}
-
+export async function GET() {
 	try {
 		const data = await getProducts();
 		return Response.json(data);
