e7a686dbf9489a61e38a963f1ce3752844003939
Abe Estrada <abe@estrada.email>
2025-10-02 08:06:10 -0600

    fix: add meta description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,8 @@ import "./globals.css";
 
 export const metadata: Metadata = {
 	title: "Business Case",
-	description: "",
+	description:
+		"Explore our test product catalog powered by DummyJSON. Browse dummy data for smartphones, laptops, fragrances, and more to test your e-commerce application.",
 };
 
 export default function RootLayout({
